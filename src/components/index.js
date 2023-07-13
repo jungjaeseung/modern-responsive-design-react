c070728d520d147ebc177da921a6387a409529a1
3Harang <jungjaeseung9475@gmail.com>
2023-07-13 17:32:22 +0900

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+import Stats from "./Stats";
+
+export {
+  Navbar,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Hero,
+  Testimonials,
+  Stats,
+};
